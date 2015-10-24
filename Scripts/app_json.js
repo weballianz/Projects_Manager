83693d177332a09e8dbf1e9fb2a11c2eb8b9a09e
Noam Bergauz <Noam Bergauz>
2015-10-24 22:10:41 +0300

    created new app.js for json test nad changed index file

---
+++ Scripts/app_json.js
@@ -0,0 +1,55 @@
+var app = angular.module('ProjectManager', ['ngRoute']);
+
+app.controller('managerController', ['$scope', '$log', function ($scope, $log) {
+    
+    $scope.tmpProjectName = '';
+    $scope.Projects_List = [];
+    
+    function Project() {
+        var name = '';
+        var taskList = [];
+        
+        function Task(name, isDone){
+            var name = name;
+            var isDone = isDone;
+            
+            this.getTaskName = function () {return name;};
+            this.getTaskStatus = function () {return isDone};
+            this.setTaskName = function (taskName) { name = taskName};
+            this.setTaskStatus = function (status) { isDone = status};
+        }
+        
+        this.getName = function () { return name};
+        this.setName = function (projectName) { name = projectName};
+        
+        this.createTask = function (taskName) {
+            var task = new Task(taskName, false);
+            taskList.push(task);
+        };
+        this.deleteTask = function (taskID) {
+            taskList[taskID].setTaskStatus = true;
+        };
+    };
+
+    $scope.addProject = function () {
+        var name = this.tmpProjectName;
+        var project = new Project();
+        project.setName(name);
+        
+        this.Projects_List.push(project);
+    };
+    
+    var RemoveItemByName = function (arr, val) {
+        var i = arr.length;
+        while (i--) {
+            var obj = arr[i];
+            if (obj.GetName() === val) arr.splice(i, 1)
+        };
+    }
+    
+    $scope.deleteProject = function (ProjectName) {
+        RemoveItemByName(this.Projects_List, ProjectName);
+    };
+    
+
+}]);
